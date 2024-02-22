faeb8853d88e46f224c10fc2e0b8ab3a8800fac0
boyc <inningbo123@sina.com>
2024-02-22 08:52:21 +0800

    更新 ts 和 eslint 相关配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,19 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   root: true,
-  extends: ["__repo__(noCase)/eslint-config/react-internal.js"],
+  extends: ["__repo__(noCase)/eslint-config/library.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: "./tsconfig.lint.json",
   },
+  rules: {
+    // '@typescript-eslint/no-explicit-any': ['off'],
+    // '@typescript-eslint/explicit-function-return-type': 'off',
+    // '@typescript-eslint/member-delimiter-style': 'off'
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-extraneous-class': 'off',
+  },
+  "env": {
+    "jest": true
+  }
 };
